748ea06780c01bba318dc9f6329f9c3806151ef0
codinronan <patrick@uncommonconcept.com>
2017-05-06 12:31:07 -0500

    Add more detailed instructions in index.js for the exercise

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ var fetch = require('isomorphic-fetch');
 var HttpStatus = require('http-status-codes');
 var app = express();
 
+/*
+1. General function to build a Reddit URL:
+  Aside from the host, it has 2 pieces:
+  https://www.reddit.com/(r/news)(/rising)/.json
+2. Use this function to return the URL just for the homepage API endpoint
+
+In the next exercise, we will use this, along with routing, to handle the data
+for ANY reddit URL! You will not have to hardcode any API endpoints.
+*/
+
 app.get('/', function (req, res) {
    console.log(req.query);
 
