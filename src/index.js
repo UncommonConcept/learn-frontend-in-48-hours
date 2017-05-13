577c4621624a3ae2eeec4450a2c94086e7c92b16
codinronan <patrick@uncommonconcept.com>
2017-05-13 01:37:00 -0500

    Add tutorial for props and prop-types

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const title = 'Welcome to React';
+const content = `<span>To get started, edit <code>src/App.js</code> and save to reload.</span>`;
+
 ReactDOM.render(
-  <App />,
+  <App title={title} content={content} />,
   document.getElementById('root')
 );
