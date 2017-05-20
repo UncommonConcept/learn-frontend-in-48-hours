6618a8298fd8fda342bfc5d5581292fc38e494c3
codinronan <codinronan@users.noreply.github.com>
2017-05-20 15:29:20 -0500

    Add the final example from the previous repo

--- src/Topics.js
+++ src/Topics.js
@@ -1,13 +1,62 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import qs from 'query-string';
 
-const Topic = ({ match, location, history }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-    <p>You answered: {(new URLSearchParams(location.search)).get('answer')}</p>
-    <button onClick={() => history.push('/')}>Go home!</button>
-  </div>
-);
+// const Topic = ({ match, location, history }) => (
+//   <div>
+//     <h3>{match.params.topicId}</h3>
+//     <p>You answered: {(new URLSearchParams(location.search)).get('answer')}</p>
+//     <button onClick={() => history.push('/')}>Go home!</button>
+//   </div>
+// );
+
+class Topic extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      extraContent: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getData(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.getData(nextProps);
+  }
+
+  getData = (props) => {
+    const { match, location, history } = props;
+    const parsed = qs.parse(location.search);
+    console.log('Searching reddit for: ', parsed.answer);
+    if(parsed.answer) {
+      fetch(`https://www.reddit.com/search.json?q=${parsed.answer}`)
+        .then(res => res.json())
+        .then((body) => {
+          this.setState({ extraContent: body })
+        })
+        .catch((error) => {
+          this.setState({ extraContent: error.message });
+        });
+    }
+  }
+
+  render() {
+    const { extraContent } = this.state;
+    const { match, location, history } = this.props;
+
+    return (
+      <div>
+        <h3>{match.params.topicId}</h3>
+        <p>You answered: {qs.parse(location.search).answer}</p>
+        <button onClick={() => history.push('/')}>Go home!</button>
+        <p>{JSON.stringify(extraContent)}</p>
+      </div>
+    );
+  }
+}
 
 const Topics = ({ match }) => (
   <div>
