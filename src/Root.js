ca0c364ddd494334d4bf95e0a38a56036a595eaf
codinronan <patrick@uncommonconcept.com>
2017-05-20 00:46:49 -0500

    Fix Root component routing so that it always renders

--- src/Root.js
+++ src/Root.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {
   BrowserRouter,
   Route,
-  Link
 } from 'react-router-dom';
 import App from './App';
 
@@ -10,9 +9,7 @@ export default class Root extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <Route exact path="/" component={App} />
-        </div>
+        <Route path="/" component={App} />
       </BrowserRouter>
     )
   }
