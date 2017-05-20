858376e57f1d0294f0cbc82475bd14dedd3c2a90
codinronan <patrick@uncommonconcept.com>
2017-05-20 01:40:08 -0500

    Complete the reddit menu

--- client/src/RedditMenu.js
+++ client/src/RedditMenu.js
@@ -8,8 +8,16 @@ class RedditMenu extends Component {
 
     return (
       <div className='Reddit-Menu'>
-        <div className='Reddit-Menu-Dropdown'>
+        <div className='Reddit-Menu-Dropdown col-md-3'>
           <span className='Reddit-Menu-Dropdown-text'>/r/{currentRedditTitle}</span>
+        </div>
+
+        <div className='Reddit-Menu-Categories col-md-9'>
+          <span className='Reddit-Menu-Category-Item'>Hot</span>
+          <span className='Reddit-Menu-Category-Item'>New</span>
+          <span className='Reddit-Menu-Category-Item'>Rising</span>
+          <span className='Reddit-Menu-Category-Item'>Controversial</span>
+          <span className='Reddit-Menu-Category-Item'>Top</span>
         </div>
       </div>
     );
