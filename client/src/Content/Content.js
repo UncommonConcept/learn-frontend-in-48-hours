3ddcbb917af649f378ee9956465cd7d265ca4252
codinronan <patrick@uncommonconcept.com>
2017-06-10 00:56:05 -0500

    Complete the transitions exercise

--- client/src/Content/Content.js
+++ client/src/Content/Content.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { Actions } from 'jumpstate';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 import { InfoColumn, MainContentColumn, RedditCommunityColumn } from './Columns';
 import MainContent from './MainContent';
@@ -16,20 +17,29 @@ const Content = (props) => {
 
   return (
     <div className="App-intro container-fluid no-padding">
-      <div className='App-layout-col-left col-md-3 no-padding'>
-        <InfoColumn />
-      </div>
+      <CSSTransitionGroup
+        transitionName="example"
+        transitionAppear={true}
+        transitionAppearTimeout={1000}
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={300}>
 
-      <div className='App-layout-col-content col-md-6'>
-        <MainContentColumn>
-          <Route path={match.path} exact component={MainContent} />
-          <Route path={`${match.path}/:category`} component={MainContent} />
-        </MainContentColumn>
-      </div>
+        <div className='App-layout-col-left col-md-3 no-padding'>
+          <InfoColumn />
+        </div>
 
-      <div className='App-layout-col-right col-md-3 no-padding'>
-        <RedditCommunityColumn />
-      </div>
+        <div className='App-layout-col-content col-md-6'>
+          <MainContentColumn>
+            <Route path={match.path} exact component={MainContent} />
+            <Route path={`${match.path}/:category`} component={MainContent} />
+          </MainContentColumn>
+        </div>
+
+        <div className='App-layout-col-right col-md-3 no-padding'>
+          <RedditCommunityColumn />
+        </div>
+
+      </CSSTransitionGroup>
     </div>
   );
 };
