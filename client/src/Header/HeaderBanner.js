b27ea3f43afaa2aa523bedd0ea8dc7a376ebbade
codinronan <patrick@uncommonconcept.com>
2017-06-10 10:24:34 -0500

    Add a sample CSS-only transition for the reddit header

--- client/src/Header/HeaderBanner.js
+++ client/src/Header/HeaderBanner.js
@@ -2,16 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const HeaderBanner = (props) => {
-  const { subreddit, bannerImage } = props;
-  const bannerLink = subreddit ? `/subreddit/${subreddit}` : '/';
+class HeaderBanner extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className='relative'>
-      <img src={bannerImage} className="App-logo" alt="avatar" />
-      <Link className='App-header-title' to={bannerLink}><h2>{subreddit || 'reddit'}</h2></Link>
-    </div>
-  );
+    this.state = {
+      addTransition: false,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ addTransition: true });
+    }, 2000);
+  }
+
+  render() {
+    const { subreddit, bannerImage } = this.props;
+    const { addTransition } = this.state;
+    const bannerLink = subreddit ? `/subreddit/${subreddit}` : '/';
+
+    const linkClass = 'App-header-title' + (addTransition ? ' transitioned' : '');
+
+    return (
+      <div className='relative'>
+        <img src={bannerImage} className="App-logo" alt="avatar" />
+        <Link className={linkClass} to={bannerLink}><h2>{subreddit || 'reddit'}</h2></Link>
+      </div>
+    );
+  }
 }
 
 HeaderBanner.propTypes = {
